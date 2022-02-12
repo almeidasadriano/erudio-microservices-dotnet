c6d4e45af5c005744991f774093a3a25c2bfdd9e
adriano.almeida <adriano.almeida@scstecnologia.com.br>
2022-02-12 06:44:56 -0300

    Initial

---
+++ ReatWithASPNET/ReatWithASPNET/Controllers/CalculatorController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReatWithASPNET.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ILogger<CalculatorController> _logger;
+
+
+
+    }
+}
