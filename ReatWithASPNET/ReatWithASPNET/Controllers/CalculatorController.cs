8d51c1cacb6b0d86b0f2dd043939b76863f2d8a4
adriano.almeida <adriano.almeida@scstecnologia.com.br>
2022-02-12 06:59:13 -0300

    Alteração

--- ReatWithASPNET/ReatWithASPNET/Controllers/CalculatorController.cs
+++ ReatWithASPNET/ReatWithASPNET/Controllers/CalculatorController.cs
@@ -9,7 +9,24 @@ namespace ReatWithASPNET.Controllers
     {
         private readonly ILogger<CalculatorController> _logger;
 
+        public CalculatorController (ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber, string secondNumber)
+        {
+            if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
 
+            }
 
+            return BadRequest("Invalid Input");
+         }
+
+        private bool IsNumeric(string firstNumber)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
